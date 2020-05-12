a0da25430af2c1d7828e54907ae0effe769c5d37
BruceAmende <bruceamende32@gmail.com>
2020-05-12 12:52:17 +0200

    taak 06 klaar

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$voornaam = "Bruce";
+$leeftijd = 17;
+$waarde = 3.55;
+$niks =;
+$waar = true;
+
+echo gettype($voornaam);
+echo gettype($leeftijd);
+echo gettype($waarde);
+echo gettype($niks);
+echo gettype($waar);
+
+
+
+
+
+?>
