0d7872985d63888efebba9826e9bfc96c22e1364
BruceAmende <bruceamende32@gmail.com>
2020-05-14 12:25:38 +0200

    taak 7/01 klaar

---
+++ 7-Include-Require/taak01/Includes/menu.php
@@ -0,0 +1,18 @@
+<div class="navb">
+            <nav>
+                
+                     <a class="active" href="index.html">Home</a>
+                     <a class="knop" href="tickets.html">Tickets</a>
+                     <a class="knop" href="prijslijst.html">Prijslijst</a>
+                     <a lass="knop" href="game.html">Game</a>
+                     
+                
+            </nav>
+
+
+
+
+
+
+
+        </div>
---
+++ 7-Include-Require/taak01/index.php
@@ -0,0 +1,31 @@
+<html>
+<head></head>
+<body>
+
+    <div class="menu">
+        <?php include 'Includes/menu.php';?>
+    </div>
+
+</body>
+</html>
+<?php
+
+
+$test1 = 12;
+$test2 = "Gewoon tekst";
+
+
+var_dump($test1 == $test2);
+var_dump($test1 === $test2);
+var_dump($test1 != $test2);
+var_dump($test1 > $test2);
+var_dump($test1 < $test2);
+var_dump($test1 <> $test2);
+var_dump($test1 >= $test2);
+var_dump($test1 <= $test2);
+
+
+
+
+
+?>
