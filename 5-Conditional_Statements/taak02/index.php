fba86f3d0333da9c0f383a1f250af3ed7cfd5832
BruceAmende <bruceamende32@gmail.com>
2020-05-14 11:54:33 +0200

    taak 6/01 klaar

---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$test1 = 12;
+$test2 = "Gewoon tekst";
+
+
+var_dump($test1 == $test2);
+var_dump($test1 === $test2);
+var_dump($test1 != $test2);
+var_dump($test1 > $test2);
+var_dump($test1 < $test2);
+var_dump($test1 <> $test2);
+var_dump($test1 >= $test2);
+var_dump($test1 <= $test2);
+
+
+
+
+
+?>
