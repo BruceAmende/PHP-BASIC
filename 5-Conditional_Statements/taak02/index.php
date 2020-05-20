e125b16840506c1c60c15b419e2ce07437dbc52e
BruceAmende <bruceamende32@gmail.com>
2020-05-20 11:20:00 +0200

    taak 5/02 klaar

--- 5-Conditional_Statements/taak02/index.php
+++ 5-Conditional_Statements/taak02/index.php
@@ -1,18 +1,37 @@
 <?php
 
 
-$test1 = 12;
-$test2 = "Gewoon tekst";
+$test1 = 10;
+$test2 = 6;
 
 
-var_dump($test1 == $test2);
-var_dump($test1 === $test2);
-var_dump($test1 != $test2);
-var_dump($test1 > $test2);
-var_dump($test1 < $test2);
-var_dump($test1 <> $test2);
-var_dump($test1 >= $test2);
-var_dump($test1 <= $test2);
+if($test1 == $test2){
+    echo "Gelijk!";
+}
+echo "<br></br>";
+if($test1 === $test2){
+    echo "Identiek!";
+}
+echo "<br></br>";
+if($test1 != $test2){
+  echo "Ongelijk!";
+}
+echo "<br></br>";
+if($test1 > $test2){
+    echo "Test 1 is groter dan Test 2!";
+}
+echo "<br></br>";
+if($test1 < $test2){
+    echo "Test 2 is groter dan Test 1";
+}
+echo "<br></br>";
+if($test1 >= $test2){
+    echo "Test 1 is groter of gelijk aan Test 2";
+}
+echo "<br></br>";
+if($test1 <= $test2){
+    echo "Test 2 is groter of gelijk aan Test 1";
+}
 
 
 
