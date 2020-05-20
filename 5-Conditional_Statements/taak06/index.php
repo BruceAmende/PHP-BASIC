4970eee6f8dce56750e75f2f5b114160551e0c8a
BruceAmende <bruceamende32@gmail.com>
2020-05-20 12:09:10 +0200

    taak 5/06 klaar

---
+++ 5-Conditional_Statements/taak06/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$test1 = 7;
+$test2 = 15;
+
+
+if($test1 > 10 && $test2 > 10  ){
+    echo "Beide getallen zijn groter dan 10";
+}else{
+    echo "Beide getallen zijn kleiner dan 10";
+}
+echo "<br></br>";
+if($test1 > 10 || $test2 <10){
+    echo "Een van de waardes is groter of kleiner dan 10";
+}else{
+    echo "Beide waardes zijn niet groter of kleiner dan 10";
+}
+echo "<br></br>";
+if($test1 > 10 xor $test2 >10){
+  echo "Een van de waardes is groter dan 10";
+}else{
+    echo "Beide waardes zijn niet groter dan 10";
+}
+echo "<br></br>";
+if($test1 < 10  ! $test2 > 10){
+    echo "Een waarde is niet groter dan 10";
+}else{
+    echo "Een waarde is wel groter dan 10";
+}
+
+
+
+?>
