0e029f625369bf5dfe750e3709e90d3829b32ff4
BruceAmende <bruceamende32@gmail.com>
2020-05-26 12:05:13 +0200

    eindopdracht PHP-BASIC  klaar

--- opdracht/includes/functions.php
+++ opdracht/includes/functions.php
@@ -1,12 +1,14 @@
 <?php
         $producten = ["Appels","Peren","Mango's","Mandarijnen","Druiven","Brocolis","Bloemkolen","Papayas","Groene kolen","Rode kolen","Koolrabies"];
         $prijzen = [0.25, 0.30, 1.20, 0.55, 0.70, 0.95, 1.40, 1.50, 1.70, 1.50, 0.35];
+       
         
 function getInput()
 {
-    $input = $_GET['input'] ?? 'vul een waarde in hierboven en klik op [check]';
+    $waarde = $_GET['input'] ?? 'vul een waarde in hierboven en klik op [check]';
         
-     echo gettype($input); 
+       echo gettype($waarde);
+     
 }
 
 
@@ -30,8 +32,29 @@ function getAge()
 }
 
 function getTotal(){
-  $totaal =  * getAmount();
-  echo $totaal;
+    $prijzen = [0.25, 0.30, 1.20, 0.55, 0.70, 0.95, 1.40, 1.50, 1.70, 1.50, 0.35];
+    $zoeken = $_GET['productnummer'];
+    $aantalProd = $_GET['aantal'];
+    
+    
+    echo $prijzen[$zoeken] * $aantalProd;
+}
+
+function getDisc(){
+    $input = $_GET['leeftijd'];
+    if($input < 16){
+        echo "1 euro";
+    }elseif($input == 19){
+        echo "19 cent";
+    }elseif($input == 18){
+        echo "18 cent";
+    }elseif($input == 17){
+        echo "17 cent";
+    }elseif($input == 16){
+        echo "16 cent";
+    }else {
+        echo "Je hebt helaas geen korting";
+    }
 }
 
 
